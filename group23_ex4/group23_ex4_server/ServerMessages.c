80299b026fc28ea967ded9c2b20c9ea335c894ae
yuval feldman <feldmanyuval@gmail.com>
2020-01-24 21:12:19 +0200

    Removed GetPlayerMove from ServerMessages

--- group23_ex4/group23_ex4_server/ServerMessages.c
+++ group23_ex4/group23_ex4_server/ServerMessages.c
@@ -38,69 +38,6 @@ int SendPlayerAloneMessage(SOCKET socket)
 	return SendMessageWithoutParams(message_name, socket);
 }
 
-int GetPlayerMove(SOCKET client_socket, MOVE_TYPE* player_move)
-{
-	int exit_code = SERVER_SUCCESS;
-	message_t* message = NULL;
-	int receive_result;
-
-	receive_result = ReceiveMessage(client_socket, &message);
-	if (receive_result != SERVER_SUCCESS)
-	{
-		if (message != NULL)
-			free(message);
-		return receive_result;
-	}
-
-	if (STRINGS_ARE_EQUAL(message->message_type, "CLIENT_PLAYER_MOVE"))
-	{
-		// Get the move of the player from the parameters
-		exit_code = ParsePlayerMoveMessage(message, player_move);
-	}
-	else
-	{
-		printf("Expected to get CLIENT_PLAYER_MOVE but got %s instead.\n", message->message_type);
-		exit_code = SERVER_UNEXPECTED_MESSAGE;
-	}
-
-	free(message);
-	return exit_code;
-}
-
-int ParsePlayerMoveMessage(message_t* message, MOVE_TYPE* player_move)
-{
-	char* player_move_string;
-
-	player_move_string = message->parameters->param_value;
-	if (STRINGS_ARE_EQUAL(player_move_string, "ROCK"))
-	{
-		*player_move = ROCK;
-	}
-	else if (STRINGS_ARE_EQUAL(player_move_string, "PAPER"))
-	{
-		*player_move = PAPER;
-	}
-	else if (STRINGS_ARE_EQUAL(player_move_string, "SCISSORS"))
-	{
-		*player_move = SCISSORS;
-	}
-	else if (STRINGS_ARE_EQUAL(player_move_string, "LIZARD"))
-	{
-		*player_move = LIZARD;
-	}
-	else if (STRINGS_ARE_EQUAL(player_move_string, "SPOCK"))
-	{
-		*player_move = SPOCK;
-	}
-	else
-	{
-		printf("Invalid move type %s.\n", player_move_string);
-		return SERVER_INVALID_PARAM_VALUE;
-	}
-
-	return SERVER_SUCCESS;
-}
-
 int SendGameResultsMessage(const char* oponent_username, MOVE_TYPE oponent_move, MOVE_TYPE player_move, const char* winner_name, SOCKET socket)
 {
 	const char* message_name = "SERVER_GAME_RESULTS";
