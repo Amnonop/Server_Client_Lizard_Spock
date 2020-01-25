aa6fcf0819c2a057211896fccbad8ec70e8a5788
Amnon Ophir <ophiramnon@gmail.com>
2020-01-25 09:19:51 +0200

    winners_linked_list

--- group23_ex4/group23_ex4_server/ServerMain.h
+++ group23_ex4/group23_ex4_server/ServerMain.h
@@ -13,6 +13,14 @@ typedef struct client_info
 	BOOL request_to_play;
 } client_info_t;
 
+typedef struct winners_list
+{
+	char* name[USERNAME_MAX_LENGTH];;
+	int won;
+	int lost;
+	struct winners_list* next;
+} winners_list_t;
+
 int RunServer(int port_number);
 
 #endif
